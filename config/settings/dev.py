5eaa9d0542a08fbf89299ca57575c36aa9d27e7f
newbieSmollett <smolletnew255@icloud.com>
2026-04-08 16:21:46 +0300

    chore: move dev settings to env

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,16 +1,23 @@
 from .base import *
+import os
+from dotenv import load_dotenv
+
+load_dotenv(BASE_DIR / ".env")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "True") == "True"
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+h-hy#mc-(9n6dq#jdu=8*-*21j&70c9#wa4!oco!7^gfizlsq"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "unsafe-development-key")
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    host.strip()
+    for host in os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+    if host.strip()
+]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
 
 try:
     from .local import *
